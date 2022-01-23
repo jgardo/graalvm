8502323f34d06f7130d7e0c26092a5ec27a6fbf9
JaGar <jakub.gardo@gmail.com>
2022-01-23 21:48:20 +0100

    Temp changes

--- graalvm-playground-javascript/src/main/resources/javascript/example.js
+++ graalvm-playground-javascript/src/main/resources/javascript/example.js
@@ -1,3 +1,15 @@
+// const { React, ReactDOMServer } = self;
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
 (str) => {
-    return str.toUpperCase();
+    class Hello extends React.Component {
+        render() {
+            return React.createElement('div', null, `Hello ${this.props.toWhat}`);
+        }
+    }
+
+    const elem = React.createElement(Hello, {toWhat: 'World'}, null)
+
+    return ReactDOMServer.renderToStaticMarkup(elem);
 }
